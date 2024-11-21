30ee8e05c3ccade0ff4332e41afc03e199a34f75
usmonjonqahhorov <qohhorovusmonjon@gmail.com>
2024-11-21 22:37:05 +0500

    database configs

--- db/config.py
+++ db/config.py
@@ -1,14 +1,12 @@
 import os
-
 from dotenv import load_dotenv
 
 load_dotenv()
 
-
 class Config:
     DB_USER = 'postgres'
-    DB_PASSWORD = 1
+    DB_PASSWORD = '1'  # Ensure this is a string
     DB_NAME = 'dombrobod_db'
-    DB_HOST = 'localhost'
-    DB_PORT = 5432
+    DB_PORT = '5432'  # Ensure this is a string
+    DB_HOST = 'postgres'  # Specify the database host
     DB_CONFIG = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
