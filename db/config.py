98d1646780fd2b165790f6c13e16096b307c5b72
Hello Sir! <qohhorovusmonjon@gmail.com>
2025-11-01 10:57:58 +0500

    additional updates

--- db/config.py
+++ db/config.py
@@ -8,5 +8,5 @@ class Config:
     DB_PASSWORD = '1'
     DB_NAME = 'dombrobod_db'
     DB_PORT = '5432'
-    DB_HOST = 'postgres'
+    DB_HOST = 'localhost'
     DB_CONFIG = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
